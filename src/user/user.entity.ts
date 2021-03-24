331394c68583e876ea2cf6f8a7894ff10eb9f070
chekkouchmehdi <61233375+chekkouchmehdi@users.noreply.github.com>
2021-03-24 15:26:29 +0100

    init project

---
+++ src/user/user.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  email: string;
+  
+}
